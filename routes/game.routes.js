79f62a1f97891296115ecc99f8d1af26f0db5599
tushar-kant <tusharkantanayak713@gmail.com>
2026-05-28 17:05:51 +0530

    fic

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -69,6 +69,7 @@ const CUSTOM_GAME_IMAGES = {
   "mlbbbr178": "https://res.cloudinary.com/dtnu1hlq9/image/upload/q_auto/f_auto/v1779962974/brazil-mlbb_rcjsfy.png",
   "mlbbtr112": "https://res.cloudinary.com/dtnu1hlq9/image/upload/q_auto/f_auto/v1779961064/mlbb-turkey_rsfqfc.png",
   "mlbb-double332": "https://res.cloudinary.com/dtnu1hlq9/image/upload/q_auto/f_auto/v1779966871/double-dias_dqv4qg.png",
+  "wuthering-of-waves464": "https://res.cloudinary.com/dtnu1hlq9/image/upload/q_auto/f_auto/v1779968054/ww_zo61px.png",
   // Add more game image mappings here...
 };
 
